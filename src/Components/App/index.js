e7c9983e2cab75328cde66d9bce41bdf6b3a555b
Christian Williams <williechri79@gmail.com>
2023-07-24 20:03:46 -0400

    file structure add and inital components have been made

---
+++ src/Components/App/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SignIn from "../SignIn";
+import './styles.css'
+
+const App = () => {
+
+    return (
+    <>
+        <SignIn />
+    </>
+    );
+  };
+  
+  export default App;

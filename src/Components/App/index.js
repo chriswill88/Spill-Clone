b1bb18b446e0a379ea930a49835b3054373b54aa
Shehu-Muhammad <shehumuhammad@my.ccsu.edu>
2023-08-03 15:13:59 -0400

    [UPDATE] added react routes to App's index.js file for better page navigation

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,14 +1,28 @@
-import React from "react";
-import SignIn from "../SignIn";
-import './styles.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
+//Pages needed: signup, login, inviteCode, basicInfo
+//My components
+import SignUp from '../SignUp';
+import SignIn from '../SignIn';
+import InviteCode from '../InviteCode';
+import BasicInfo from '../BasicInfo';
+// import 
 const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/inviteCode" element={<InviteCode />} />
+        <Route path="/basicInfo" element={<BasicInfo />} />
+        {/* <Route path="/users" element={<Users />} /> */}
+      </Routes>
+    </Router>
+  );
+};
 
-    return (
-    <>
-        <SignIn />
-    </>
-    );
-  };
-  
-  export default App;
+export default App;
+
+
+
